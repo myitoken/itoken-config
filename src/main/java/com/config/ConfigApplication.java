98225b7796ee8c6be9b1208db7bce77b5b2aa01a
fengqiao“git commit -m first <1404369980@qq.com>
2020-01-02 17:22:21 +0800

    sd

---
+++ src/main/java/com/config/ConfigApplication.java
@@ -0,0 +1,22 @@
+package com.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author 易桥锋
+ * @version 1.0
+ * @date 2020/1/2 0002 16:02
+ * @Description
+ */
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class,args);
+    }
+}
